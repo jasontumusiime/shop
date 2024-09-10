518e0072d258f5d689f763d074c7690dd2571059
Jason Tumusiime <jason.2mcme@gmail.com>
2024-09-10 17:56:07 +0300

    Add password recovery feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,11 @@ from . import views
 urlpatterns = [
   path('activate/<uidb64>/<token>/', views.activate, name='activate'),
   path('dashboard/', views.dashboard, name='dashboard'),
+  path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
   path('register/', views.register, name='register'),
-  path('/', views.dashboard, name='account'),
+  path('resetPassword/', views.resetPassword, name='resetPassword'),
+  path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+  path('', views.dashboard, name='account'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
 from django.utils.encoding import force_bytes
@@ -98,3 +99,69 @@ def activate(request, uidb64, token):
 @login_required(login_url= 'login')
 def dashboard(request):
   return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+  if request.method == 'POST':
+    
+    email = request.POST['email']
+    if Account.objects.filter(email=email).exists():
+      user = Account.objects.get(email__exact=email)
+      print('User ID: ' + str(user.pk))
+      # Password Recovery
+      current_site = get_current_site(request)
+      mail_subject = "Reset your password"
+      message = render_to_string('accounts/reset_password_email.html', {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
+      })
+      to_email = email
+      mailer = EmailMessage(mail_subject, message, to=[to_email])
+      mailer.send()
+
+      messages.success(request, 'Password reset email has been sent your email')
+      return redirect('login')
+  
+    else:
+      messages.error(request, "Account does not exist")
+      return redirect('forgotPassword')
+
+  return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+  try:
+    uid = urlsafe_base64_decode(uidb64)
+    user = Account.objects.get(pk=uid)
+  except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+    user = None
+
+  if user and default_token_generator.check_token(user, token):
+    request.session['uid'] = uid
+    messages.success(request, "Please reset your password")
+    return redirect('resetPassword')
+  else:
+    messages.error(request, 'This link has been expired')
+    return redirect('login')
+  
+
+def resetPassword(request):
+  if request.method == 'POST':
+    password = request.POST['password']
+    re_password = request.POST['cofirm_password']
+
+    if re_password == password:
+      uid = request.session.get('uid')
+      user = Account.objects.get(pk=uid)
+      user.set_password(password)
+      user.save()
+
+      messages.success(request, 'Password reset successful')
+      return redirect('login')
+    else:
+      messages.error(request, "Passwords do not match")
+      return redirect('resetPassword')
+    
+  return render(request, 'accounts/resetPassword.html')
